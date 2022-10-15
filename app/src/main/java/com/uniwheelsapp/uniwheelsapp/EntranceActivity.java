5dc106f63d46655871dd2dcc65bfcb2a7a346919
Sergio Andrés Bolaños Penagos <sergio.bolanos@imagineapps.co>
2022-10-15 18:37:43 -0500

    Prueba error de firebase al dar click en botón

--- app/src/main/java/com/uniwheelsapp/uniwheelsapp/EntranceActivity.java
+++ app/src/main/java/com/uniwheelsapp/uniwheelsapp/EntranceActivity.java
@@ -6,17 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class EntranceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entrance);
+
+        FirebaseAnalytics firebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        Bundle bundleAnalytics = new Bundle();
+        bundleAnalytics.putString("message", "Integración con firebase");
+        firebaseAnalytics.logEvent("InitScreen", bundleAnalytics);
     }
 
     public void onGoLogin(View view){
-        Intent i = new Intent(this, LoginActivity.class);
-        startActivity(i);
+        throw new RuntimeException("Test Crash");
+        // Intent i = new Intent(this, LoginActivity.class);
+        // startActivity(i);
     }
 
     public void onGoRegister(View view){
