141f9e2456f3208df76de2edce1453044f66aa13
Sergio Andrés Bolaños Penagos <sergio.bolanos@imagineapps.co>
2022-10-15 00:10:15 -0500

    WA-10 registro y logout con google completado

--- app/src/main/java/com/uniwheelsapp/uniwheelsapp/MainActivity.java
+++ app/src/main/java/com/uniwheelsapp/uniwheelsapp/MainActivity.java
@@ -1,17 +1,58 @@
 package com.uniwheelsapp.uniwheelsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView name, email;
+    Button logout;
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        System.out.println("HACE ESTOOOOO");
+        if(account != null){
+            String name = account.getDisplayName();
+            String mail = account.getEmail();
+            System.out.println("Nombre " + name);
+            System.out.println("Email " + mail);
+        }
+
+        logout = findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignOut();
+            }
+        });
     }
 
     public void onGoNow(View view){
@@ -27,5 +68,15 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception exception) {
             System.out.println(exception);
         }
+    }
+
+    private void SignOut(){
+        gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                finish();
+                startActivity(new Intent(getApplicationContext(),EntranceActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/uniwheelsapp/uniwheelsapp/RegisterActivity.java
+++ app/src/main/java/com/uniwheelsapp/uniwheelsapp/RegisterActivity.java
@@ -2,13 +2,70 @@ package com.uniwheelsapp.uniwheelsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import javax.annotation.Nullable;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    ImageView googleButton;
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        googleButton = findViewById(R.id.googleButton);
+        gso = new GoogleSignInOptions.Builder()
+                .requestEmail()
+                .build();
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                SignIn();
+            }
+        });
+    }
+
+    public void SignIn(){
+        Intent intent = gsc.getSignInIntent();
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 100){
+            try {
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                task.getResult(ApiException.class);
+                MainActivity();
+            } catch (ApiException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void MainActivity(){
+        finish();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
